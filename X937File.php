e90ee49ebd7346c2d1c90cc2424b00efa3e1c36f
Austin Stanley <astanley@cnbtxk.com>
2013-12-05 17:55:07 -0600

    Added ASCII/EBCDIC type checking. Refactored and eliminated some unnecessary code.

--- X937File.php
+++ X937File.php
@@ -3,10 +3,17 @@
 require_once 'X937Record.php';
 require_once 'X937RecordFactory.php';
 require_once 'X937Field.php';
-
+/**
+ * An X937File
+ * 
+ * @todo Test ASCII file codepaths. Currently untested as have no example work.
+ */
 class X937File implements Countable, Iterator {
     const DATA_ASCII  = 'ASCII';
     const DATA_EBCDIC = 'EBCDIC-US';
+    
+    const FILE_SIGNATURE_ASCII  = '3031';
+    const FILE_SIGNATURE_EBCDIC = 'f0f1';
     
     /**
      * Our FileHandle.
@@ -33,16 +40,16 @@ class X937File implements Countable, Iterator {
     private $currentRecordPosition;
     
     /**
-     * Current record
-     * @var X937Record
-     */
-    private $currentRecord;
-    
-    /**
      * Current record length
      * @var int
      */
     private $curentRecordLength;
+    
+    /**
+     * File Header Record for the File
+     * @var X937RecordFileHeader
+     */
+    private $fileHeaderRecord;
     
     /**
      * File Control Record for the File
@@ -82,12 +89,46 @@ class X937File implements Countable, Iterator {
 	// open our file for reading in binary mode.
 	$this->fileHandle = fopen($filename, 'rb');
 	
+	// check file data type. According to doc this is done by checking the
+	// first field in the first record (that is File Header Record - Type 01
+	// Field 01 - Record Type). Should be 0xf0f1 for EBCDIC and 0x3031 for
+	// ASCII. If neither of those, we got bad data probably.
+
+	// seek to the right location. 4 bytes from the start of the file.
+	fseek($this->fileHandle, 4, SEEK_SET);
+	
+	// parse the first two bytes, which contain the appropriate record.
+	$initialRecordDataTypeRaw = fread($this->fileHandle, 2);
+	$initialRecordDataTypeHex = bin2hex($initialRecordDataTypeRaw);
+	
+	// switch on that data, comparing them to the known hex values.
+	switch ($initialRecordDataTypeHex) {
+	    case self::FILE_SIGNATURE_EBCDIC:
+		$this->dataType = self::DATA_EBCDIC;
+		break;
+	    case self::FILE_SIGNATURE_ASCII:
+		$this->dataType = self::DATA_ASCII;
+		break;
+	    default:
+		throw new InvalidArgumentException('Unable to parse file, bad data given.');
+		break;
+	}
+	
+	// rewind to the start for the other stuff.
+	rewind($this->fileHandle);
+	
+	// pull the first record, this should always be the file header record.
+	$this->fileHeaderRecord = $this->current();
+	
+	if (($this->fileHeaderRecord instanceof X937RecordFileHeader) === FALSE) {
+	    throw new InvalidArgumentException('Bad file given, first record is not a File Header Record.');
+	}
+	
 	/**
-	 * @todo add record type check here, DATA_ASCII or DATA_EBCDIC
+	 * @todo see if we can figure out how to parse records backwards elegantly, and use that here instead.
 	 */
-	$this->dataType = self::DATA_EBCDIC;
-		
-	// seek to 80 characters before the end of file. This SHOULD give us the file control recored.
+
+	// seek to 80 characters before the end of file. This should always be the File Control Record.
 	fseek($this->fileHandle, -80, SEEK_END);
 	
 	// read those 80 characters
@@ -99,6 +140,8 @@ class X937File implements Countable, Iterator {
 	    $this->fileTotalAmount = $this->fileControlRecord->getFieldByNumber(5)->getValue()/100;
 	    $this->fileItemCount   = $this->fileControlRecord->getFieldByNumber(4)->getValue();
 	    $this->recordCount     = $this->fileControlRecord->getFieldByNumber(3)->getValue();
+	} else {
+	    throw new InvalidArgumentException('Bad file given, last record is not a File Control Record.');
 	}
 	
 	// rewind the file pointer;
@@ -112,31 +155,7 @@ class X937File implements Countable, Iterator {
 
     public function getFileTotalAmount() { return $this->fileTotalAmount; }
     public function getFileItemCount()   { return $this->fileItemCount; }
-	
-    public function readRecord() {
-        // save our record position
-        $this->recordPositions = ftell($this->fileHandle);
-            
-        // pull 4 bytes, this should contain our record length.
-	$recordLengthData = fread($this->fileHandle, 4);
-		
-	// check to see if we have a value here. If not, we've reached the eof, and just return
-	// if (!$recordLengthData) { return false; }
-		
-	// unpack our data into an int, unpack should return an with a single value
-	// i.e. array '['int']=>RECORDLENGTH so array shift will get us the raw value.
-	$this->curentRecordLength = array_shift(unpack("Nint", $recordLengthData));
-
-	// read the data for our record. Build a record.
-	$recordData = fread($this->fileHandle, $this->curentRecordLength);	
-	$record     = X937RecordFactory::newRecordFromRawData($recordData, $this->dataType);
-	
-	// seek back to the old position
-	fseek($this->fileHandle, -$this->curentRecordLength, SEEK_CUR);
-
-	return $record;
-    }
-
+    
     /**
      * Implementation for countable. Returns the number of records from the file
      * control record. Be aware that if that count is wrong, this count is wrong.
@@ -168,8 +187,27 @@ class X937File implements Countable, Iterator {
      * @return X937Record The current x937Record
      */
     public function current() {
-	$this->currentRecord = $this->readRecord();
-	return $this->currentRecord;
+	        // save our record position
+        $this->recordPositions = ftell($this->fileHandle);
+            
+        // pull 4 bytes, this should contain our record length.
+	$recordLengthData = fread($this->fileHandle, 4);
+		
+	// check to see if we have a value here. If not, we've reached the eof, and just return
+	// if (!$recordLengthData) { return false; }
+		
+	// unpack our data into an int, unpack should return an with a single value
+	// i.e. array '['int']=>RECORDLENGTH so array shift will get us the raw value.
+	$this->curentRecordLength = array_shift(unpack("Nint", $recordLengthData));
+
+	// read the data for our record. Build a record.
+	$recordData = fread($this->fileHandle, $this->curentRecordLength);	
+	$record     = X937RecordFactory::newRecordFromRawData($recordData, $this->dataType);
+	
+	// seek back to the old position
+	fseek($this->fileHandle, -$this->curentRecordLength, SEEK_CUR);
+
+	return $record;
     }
     
     public function next() {
