aa8b58ff0699fb28507c2f7bd72d38a9a1353f4f
astanley <astanley@DT-OFFICE3.cnbtxk.local>
2017-06-22 14:09:33 -0500

    Removed more dead code, changed some internal of Field implementation added bones of a Bundle/Cashletter implementation.

---
+++ Bundle.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace X937;
+
+/**
+ * Description of Bundle
+ *
+ * @author astanley
+ */
+class Bundle extends Container {
+    private $records;
+    
+    public function __construct(array $records) {
+        $this->records = $records;
+    }
+    
+    public function validate(): string {
+        $error = '';
+        
+        foreach($this->records as $record) {
+            $error .= $record->validate();
+        }
+        
+        return $error;
+    }
+}
